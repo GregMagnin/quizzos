1aff9ba74dbaf1e61828bb2bb8423a5d50cf8225
thibault <t.guignand@codeur.online>
2022-11-08 14:47:21 +0100

    fix:lobby page optimisation

--- src/animations/progressBar.js
+++ src/animations/progressBar.js
@@ -1,28 +1,25 @@
-let progressBar = document.getElementById("js-progress-bar");
-let valueProgressBar = document.getElementById("js-value-progress-bar");
+let progressBar = document.getElementById('js-progress-bar');
+let valueProgressBar = document.getElementById('js-value-progress-bar');
 
-let goodAnswer = parseInt(localStorage.getItem("number_good_answers"));
-let nbOfQuestions = parseInt(localStorage.getItem("questions"));
-let goodAnswerText = document.getElementById("goodAnswer");
-goodAnswerText.innerText = localStorage.getItem("number_good_answers");
-let nbOfQuestionsText = document.getElementById("nbOfQuestions");
-nbOfQuestionsText.innerText = localStorage.getItem("questions");
+let goodAnswer = parseInt(localStorage.getItem('number_good_answers'))
+let nbOfQuestions = parseInt(localStorage.getItem('questions'))
+let goodAnswerText = document.getElementById('goodAnswer');
+goodAnswerText.innerText = localStorage.getItem('number_good_answers');
+let nbOfQuestionsText = document.getElementById('nbOfQuestions');
+nbOfQuestionsText.innerText = localStorage.getItem('questions');
 
 let progressValue = 0;
-let speedAnimation = 25;
 let progressEndValue = Math.trunc((goodAnswer / nbOfQuestions) * 100);
-console.log(progressEndValue);
+let speedAnimation = 25;
 
-if (goodAnswer > 0) {
-    let progressAnimation = setInterval(() => {
-        progressValue++;
-        valueProgressBar.textContent = `${progressValue}%`;
-        progressBar.style.background = `conic-gradient(
-            #ED8681 ${progressValue * 3.6}deg,
-            #e0d3d5 ${progressValue * 3.6}deg
-        )`;
-        if (progressValue === progressEndValue) {
-            clearInterval(progressAnimation);
-        }
-    }, speedAnimation);
-}
+let progressAnimation = setInterval(() => {
+    progressValue++;
+    valueProgressBar.textContent = `${progressValue}%`;
+    progressBar.style.background = `conic-gradient(
+        #ED8681 ${progressValue * 3.6}deg,
+        #e0d3d5 ${progressValue * 3.6}deg
+    )`;
+    if (progressValue == progressEndValue) {
+        clearInterval(progressAnimation)
+    }
+}, speedAnimation);
--- src/scripts/categories-fetch.js
+++ src/scripts/categories-fetch.js
@@ -1,7 +1,7 @@
 let categories = document.getElementById('categories');
 
 
-export let categoriesfetch = categories.addEventListener('click', async (event) => {
+categories.addEventListener('click', async (event) => {
     let categorievalue = event.target.value;
     localStorage.setItem('categorie', categorievalue);
     document.location.href = '#lobby';
